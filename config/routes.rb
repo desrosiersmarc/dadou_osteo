b0267e39d980f050fbf9a9e8b760ae11f40c0f9e
marc desrosiers <mdesrosiers@norauto.fr>
2018-03-05 08:10:59 +0100

    Update controller and view to create a new appointment for a customer

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :appointment_types
 
-  resources :appointments
+  resources :appointments, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
