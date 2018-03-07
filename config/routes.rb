8c4f1df05bce20abdd3ee414d63b14e51eed7629
marc desrosiers <mdesrosiers@norauto.fr>
2018-03-07 22:38:23 +0100

    work on customer appointments, test a gemfile for display stars

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   resources :appointments, only: [:index, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'my_appointments', to: 'appointments#my_appointments'
 end
